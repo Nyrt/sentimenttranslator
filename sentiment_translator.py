fa8b3a8ee60d3f3049d4a6d0838a1de04a3bda75
nwatts02 <nathan.watts@tufts.edu>
2017-12-01 14:33:27 -0500

    added sentiment target

--- sentiment_translator.py
+++ sentiment_translator.py
@@ -7,6 +7,7 @@ from nltk.parse.stanford import StanfordParser
 from nltk.tag.stanford import StanfordPOSTagger
 import string
 import os
+import re
 
 os.environ["CLASSPATH"] = "1"
 
@@ -65,34 +66,21 @@ tagger = StanfordPOSTagger(pos_tag_model, pos_tag_jar)
 
 
 #Tag words and split at conjunctions
-def parse_tag(words):
-	clauses = []
-	tags = tagger.tag(words)
-	clause_start = 0
-	clause_end = 0
-	for word, tag in tags:
-		clause_end += 1
-		if tag == 'CC' or tag == 'IN':
-			clauses.append(tags[clause_start:clause_end])
-			clause_start = clause_end
-	if clause_start != clause_end:
-		clauses.append(tags[clause_start:])
+def parse_tag(text):
+	sentences = text.split(".?!")
+	clauses = [tagger.tag(sentence.split()) for sentence in sentences]
+	# tags = tagger.tag(words)
+	# clause_start = 0
+	# clause_end = 0
+	# for word, tag in tags:
+	# 	clause_end += 1
+	# 	if tag == 'CC' or tag == 'IN' or word[-1] in string.punctuation:
+	# 		clauses.append(tags[clause_start:clause_end])
+	# 		clause_start = clause_end
+	# if clause_start != clause_end:
+	# 	clauses.append(tags[clause_start:])
+	print clauses
 	return clauses
-
-	# parser = StanfordParser()
-	# parse_str = parser.parse(sentence)
-	# print parse_str
-	# clauses = []
-	# for subtree in Tree.fromstring(parse_str).subtrees():
-	# 	if subtree.label() == "S" or subtree.label() == "SBAR":
-	# 		clauses.append(''.join(subtree.leaves()))
-	# clauses_bck = clauses[:]
-	# print clauses
-	# for i in reversed(range(len(clauses) - 1)):
-	# 	clauses[i] = clauses[i][0:clauses[i].index(clauses[i+1])]
-	# clauses.append(clauses_bck[0][clauses[0].index(clauses[1]) + len(clauses[1]):])
-	# for clause in clauses:
-	# 	print clauses
 
 
 def wn_tag(treebank_tag):
@@ -107,54 +95,83 @@ def wn_tag(treebank_tag):
 	else:
 		return ''
 
-def Translate(words):
+# Returns sentiment in a range from -1 to 1 (negative to positive respectively)
+def sentiment(sentence):
+	sid = SentimentIntensityAnalyzer()
+	ss = sid.polarity_scores(sentence)
+	return (ss['pos'] - ss['neg']) * (1.0-ss['neu'])
+
+def invert_stopwords(word):
+	if word == "not":
+		return ""
+	elif word == "dont":
+		return "do"
+	elif word == "cant":
+		return "can"
+	elif word == "wont":
+		return "will"
+	elif word == "shouldnt":
+		return "should"
+	elif word == "can":
+		return "can't"
+	elif word == "should":
+		return "shouldn't"
+	elif word == "will":
+		return "won't"
+	elif word == "but":
+		return "and"
+	elif word == "and":  # Check the tags!
+		return "or"
+	elif word == "or":
+		return "and"
+	else:
+		return word
+
+# Words is the tagged output of parse_tag
+# Target is either 1 (positive) or -1 (negative)
+def Translate(words, target):
+	new_clauses = []
 	#print words
+	n_clause = len(words)
+	i_clause = 0
 	for clause in words:
+		i_clause+= 1
+		print "optimizing clause %i out of %i"%(i_clause, n_clause)
 		new_clause = [word[0] for word in clause]
+		best_sentiment = sentiment(" ".join(new_clause))
+		print best_sentiment
 		for i in xrange(len(clause)):
-			word = strip(clause[i][0])
+			word = clause[i][0]
 			tag = clause[i][1]
 			s_tag = wn_tag(tag)
 
 			# Remove inverters
-			# FIX STRING COMPARISON-> UNICODE STRINGS
-			if word == "not":
-				new_clause[i] = ""
-				continue
-
-			if word == "dont":
-				new_clause[i] = "do"
-				continue
-
-			if word == "cant":
-				new_clause[i] = "can"
-				continue
-
-			if word == "wont":
-				new_clause[i] = "will"
-				continue
-
-			# We can't have any differing sentiments!
-			if word == "but":
-				new_clause[i] = "and"
-				continue
+			new_clause[i] = invert_stopwords(word)
 			
 			# Replace adjectives with antonyms
 			if word in stopwords.words():
 				continue
 
 			anti_found = False
-			for w in wn.synsets(word, pos=s_tag):
+
+			# Preserve trailing punctuation 
+			punc = []
+			if word[-1] in string.punctuation:
+				punc = [c for c in word if c in string.punctuation]
+			print punc
+			for w in wn.synsets(strip(word), pos=s_tag):
 				for j in w.lemmas():
 					if j.antonyms():
 
 						#Conjugate words???
-						new_clause[i] = " ".join(j.antonyms()[0].name().split("_")) + '`'
+						new_clause[i] = " ".join(j.antonyms()[0].name().split("_"))
+						if len(punc) > 0:
+							new_clause[i] += "".join(punc)
 						anti_found = True
 						break
 				if anti_found:
 					break
-			
+
 			# If the word has no antonym, invert it.
 			if not anti_found and (s_tag == wn.ADJ or s_tag == wn.ADV):
 				new_clause[i] = "not " + word
@@ -163,22 +180,42 @@ def Translate(words):
 				new_clause[i] = "didn't " + word
 
 			# Check if the sentiment has gone in the right direction
+			print new_clause
+			new_sentiment = sentiment(" ".join(new_clause))
+			change_score = (best_sentiment - new_sentiment) * target
+			if change_score <= 0:
+				new_clause[i] = word
+			else:
+				best_sentiment = new_sentiment
+				print best_sentiment
 
 			# If not, revert the change.
 
-		print " ".join(new_clause)
+
+		new_clauses.append(new_clause)
+	return new_clauses
 
 
 sentence = raw_input("Enter a sentence\n")
 
-words = sentence.split()
+target = ""
 
-tags = parse_tag(words)
+while target not in ['n', 'p']:
+	target = raw_input("Enter a target sentiment (p: pos, n: neg)")
+if target == 'n':
+	target = 1
+else:
+	target = -1
 
-Translate(tags)
 
-#sentim_toy(sentence)
+tags = parse_tag(sentence)
 
+new_words = Translate(tags, target)
+
+print " ".join(" ".join(clause) for clause in new_words)
+
+
+#NOTE: Fix punctuation! Doesn't split at periods. 
 
 
 
